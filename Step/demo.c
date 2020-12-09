5a13243f39be69b93f85511ef78d35063adad336
lcx18 <root@PC-202003171526.localdomain>
2020-12-09 20:30:41 +0800

    commit file demo.c

--- Step/demo.c
+++ Step/demo.c
@@ -9,7 +9,7 @@ int add(int a, int b)
     int i;
     int array1[10] ={11, 22, 33, 44, 55, 66, 77, 88, 99, 00};
     int array2[10] ={1, 2, 3, 4, 5, 4, 3, 2, 1, 0};
-    for (i = 0; i < 10; i++)
+    for (i = 0; i < 10; i++)/*  */
       {
         sum[i] = add(array1[i], array2[i]);
       }
